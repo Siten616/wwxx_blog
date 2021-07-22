9a3208765a0fc21f951ba0af7c410e9efd90660c
Siten616 <1119388709@qq.com>
2021-07-22 16:25:46 +0800

    Initial commit

---
+++ blog-admin/src/main/java/com/siten/blog/BlogAdminApplication.java
@@ -0,0 +1,15 @@
+package com.siten.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动入口
+ * Created by siten on 2021/7/22.
+ */
+@SpringBootApplication
+public class BlogAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAdminApplication.class, args);
+    }
+}
